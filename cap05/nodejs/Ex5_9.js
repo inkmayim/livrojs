dfba1be529b88ce18ac356457868192cdc562039
benderrw <benderrw@gmail.com>
2025-09-05 10:54:35 -0300

    Adiciona novos arquivos JavaScript para o programa de anos de Copa do Mundo e para a manipulação de etiquetas de produtos.

---
+++ cap05/nodejs/Ex5_9.js
@@ -0,0 +1,6 @@
+const prompt = require("prompt-sync")()
+const produto = prompt("Produto: ")
+const num = Number(prompt("Nº de Etiquetas: "))
+for (let i = 1; i <= num / 2; i++) {
+  console.log 
+}
